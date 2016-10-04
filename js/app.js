12d7c5da4ef6a485e9274202f6ca1a6729ec0b5b
Tse Kit Yam <me@kytse.com>
2016-10-05 00:22:01 +0800

    Introduce model object
    Model object keeps a list of student instances. It also provide
    interface for managing student instances in local storage.

--- js/app.js
+++ js/app.js
@@ -23,3 +23,42 @@ Student.prototype.toggleMissing = function (day) {
         this.attendance[day] = !this.attendance[day];
     }
 };
+
+// the model used to store the student objects.
+var model = {
+    students: []
+};
+
+model.init = function() {
+    this.students.push(new Student("Honoka Kosaka"));
+    this.students.push(new Student("Eli Ayase"));
+    this.students.push(new Student("Kotori Minami"));
+    this.students.push(new Student("Umi Sonoda"));
+    this.students.push(new Student("Rin Hoshizora "));
+    this.students.push(new Student("Maki Nishikino"));
+    this.students.push(new Student("Nozomi Tojo"));
+    this.students.push(new Student("Hanayo Koizumi"));
+    this.students.push(new Student("Nico Yazawa"));
+};
+
+model.getStudentByName = function(name) {
+    for (student of this.students) {
+        if (student.name === name) {
+            return student;
+        }
+    }
+};
+
+model.saveToLocalStorage = function() {
+    localStorage.students = JSON.stringify(this.students);
+};
+
+model.loadFromLocalStorage = function() {
+    this.students = JSON.parse(localStorage.students);
+};
+
+model.clearLocalStorage = function() {
+    localStorage.clear();
+};
+
+model.init();
