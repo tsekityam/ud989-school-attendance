399dd4df104ef93a955b1baf05f1b1bdcd85ae64
Tse Kit Yam <me@kytse.com>
2016-10-05 22:41:44 +0800

    Remove object functions
    These functions are not available in the octopus. However, they are not
    in use, so we can remove them now and see how to fix it later.

--- js/app.js
+++ js/app.js
@@ -8,22 +8,6 @@ var Student = function(name) {
     }
 }
 
-Student.prototype.getMissingCount = function () {
-    var missingCount = 0;
-    for (var i = 0; i < this.attendance.length; i++) {
-        if (this.attendance[i] == false) {
-            missingCount++;
-        }
-    }
-    return missingCount;
-};
-
-Student.prototype.toggleMissing = function (day) {
-    if (day < this.attendance.length) {
-        this.attendance[day] = !this.attendance[day];
-    }
-};
-
 // the model used to store the student objects.
 var model = {
     students: []
